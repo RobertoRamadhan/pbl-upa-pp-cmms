e847e1d02371eadd0726860957e8de583506b73b
RobertoRamadhan <robertonovember.r@gmail.com>
2025-12-31 05:58:49 +0700

    Disable problematic ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,13 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "prefer-const": "off",
+      "@next/next/no-img-element": "off",
+    },
   },
 ];
 
